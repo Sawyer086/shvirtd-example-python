7de10b156339955ad22e8b9e7d7a12d00282b80f
Sawyer086 <sergey-punin@mail.ru>
2025-03-09 15:06:22 +0500

    add 5 files

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d "/opt/shvirtd-example-python" ] ; then
+    sudo git clone https://github.com/Sawyer086/shvirtd-example-python /opt/shvirtd-example-python
+else
+    cd /opt/shvirtd-example-python
+    sudo git pull
+fi
+
+pwd
+
+sudo docker compose -f compose.yaml up
